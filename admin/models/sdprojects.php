5ef34af4372136af4c4f4cbe3a57dd593d8890d0
Jason Mayhew <jmayhew@mail.utexas.edu>
2016-04-08 15:24:19 -0500

    Changed select statement to select(array....

--- admin/models/sdprojects.php
+++ admin/models/sdprojects.php
@@ -53,8 +53,9 @@ class SdprojectsModelSdprojects extends JModelList
 		// 		->from($db->quoteName('#__projects', 'a'))
 		// 		->join('LEFT', $db->quoteName('#__sponsors', 'b') .' ON (' . $db->quoteName('a.company') . ' = ' . $db->quoteName('b.id') . ')')
 		// 		->where($db->quoteName('a.company') . ' = ' . ('b.id'));
-
-		$query->select('*, b.name')
+		
+		$query->select(array('a.*', 'b.id', 'b.name'))
+		//$query->select('*, b.name')//
 				->from($db->quoteName('#__projects'))
 				->join('LEFT', $db->quoteName('#__sponsors') .' ON (' . $db->quoteName('a.company') . ' = ' . $db->quoteName('b.id') . ')')
 				->where($db->quoteName('a.company') . ' = ' . ('b.id'));
