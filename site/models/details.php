2c43b5df219b399c7fde9d5e8320d75a860fc1d1
Jason Mayhew <jmayhew@mail.utexas.edu>
2016-04-13 11:58:13 -0500

    Added getQuery statement

--- site/models/details.php
+++ site/models/details.php
@@ -35,7 +35,9 @@ class SdprojectsModelDetails extends JModelItem
 	public function getSdp()
 	{
 		$db =& JFactory::getDBO();
-		$id = JRequest::getInt('id');	
+		$id = JRequest::getInt('id');
+		
+		$query = $db->getQuery(true);	
  		$query->select(array('a.*', 'b.id', 'b.name'))
 				->from($db->quoteName('#__projects', 'a'))
 				->join('LEFT', $db->quoteName('#__sponsors', 'b') .' ON (' . $db->quoteName('a.company') . ' = ' . $db->quoteName('b.id') . ')')
