565c1ec95a4104363fe2ef58141cf96acba5c8cc
Jason Mayhew <jmayhew@mail.utexas.edu>
2016-04-13 12:25:52 -0500

    Changed var name in view to reflect adjusted column names

--- site/views/default/tmpl/default.php
+++ site/views/default/tmpl/default.php
@@ -12,7 +12,7 @@ foreach($this->items as $row):
 <div class="sdp">
 	<p class="sdp-title"><a href="<?php echo JRoute::_('index.php?view=details&id='.$row->id); ?>"><?php echo $row->title ?></a></p>
 	<p class="sdpstudents"><?php echo $row->students ?></p>
-	<p class="sdpsponsors"><?php echo $row->name ?></p>
+	<p class="sdpsponsors"><?php echo $row->coname ?></p>
 	<p class="sdpsemester"><?php echo $row->semester ?> <?php echo $row->year ?></p>
 </div>
 <?php endforeach; ?>
