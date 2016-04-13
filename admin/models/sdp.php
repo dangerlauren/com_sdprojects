2a8be71190c04d1c3437022f9e744cce811cddc2
Grant <grantlauren@wireless-10-145-134-178.public.utexas.edu>
2016-04-13 15:31:05 -0500

    tried splitting up the select query in admin details view

--- admin/models/sdp.php
+++ admin/models/sdp.php
@@ -51,7 +51,9 @@ class SdprojectsModelSdp extends JModelAdmin
 		$id = JRequest::getInt('id');
 		
 		$query = $db->getQuery(true);	
- 		$query->select(array('a.*', 'b.id', 'b.name'))
+ 		$query->select(array('a.*'))
+ 		->select($db->quoteName('b.id', 'coid'))
+		->select($db->quoteName('b.name', 'coname'))
 				->from($db->quoteName('#__projects', 'a'))
 				->join('LEFT', $db->quoteName('#__sponsors', 'b') .' ON (' . $db->quoteName('a.company') . ' = ' . $db->quoteName('b.id') . ')')
 				->where($db->quoteName('a.id') . ' = ' . ($id));
