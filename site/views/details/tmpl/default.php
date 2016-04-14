973a36a8270c616436662bc345aaa8f67c11a673
Grant <grantlauren@wireless-10-145-250-102.public.utexas.edu>
2016-04-14 09:37:14 -0500

    made company field a list in xml file, altered layout of site detail view

--- site/views/details/tmpl/default.php
+++ site/views/details/tmpl/default.php
@@ -3,9 +3,9 @@
 <div id="sdprojects">
 	<h1><?php echo $this->sdp->title ?></h1>
 	<div class="sdpdetails">
-		<p class="sdpstudents"><b>Students:</b><br /><?php echo $this->sdp->students ?></p>
-		<p class="sdpsponsors"><b>Sponsor:</b><br /><a href="<?php echo $this->sdp->url ?>"><?php echo $this->sdp->name ?></a></p>
-		<p class="sdpsemester"><b>Date:</b><br /><?php echo $this->sdp->semester ?> <?php echo $this->sdp->year ?></p>
+		<p class="sdpstudents"><b>Students: </b><?php echo $this->sdp->students ?></p>
+		<p class="sdpsponsors"><b>Sponsor: </b><a href="<?php echo $this->sdp->url ?>"><?php echo $this->sdp->name ?></a></p>
+		<p class="sdpsemester"><b>Date: </b><?php echo $this->sdp->semester ?> <?php echo $this->sdp->year ?></p>
 		<p class="sdpreqs"><b>Requirements:</b><br /><?php echo $this->sdp->reqs ?></p>
 		<p class="sdpproblem"><b>Problem:</b><br /><?php echo $this->sdp->problem ?></p>
 		<p class="sdpsolution"><b>Solution:</b><br /><?php echo $this->sdp->solution ?></p>
