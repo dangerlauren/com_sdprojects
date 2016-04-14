1eb3710be990695f4e8c367f6f30a1849e3177b0
Grant <grantlauren@wireless-10-145-250-102.public.utexas.edu>
2016-04-14 09:53:42 -0500

    added if statment around company name to link if available

--- site/views/details/tmpl/default.php
+++ site/views/details/tmpl/default.php
@@ -4,7 +4,13 @@
 	<h1><?php echo $this->sdp->title ?></h1>
 	<div class="sdpdetails">
 		<p class="sdpstudents"><b>Students: </b><?php echo $this->sdp->students ?></p>
+
+		<?php if ($this->sdp->url == !NULL): ?>
 		<p class="sdpsponsors"><b>Sponsor: </b><a href="<?php echo $this->sdp->url ?>"><?php echo $this->sdp->name ?></a></p>
+		<?php elseif ?>
+		<p class="sdpsponsors"><b>Sponsor: </b><?php echo $this->sdp->name ?></a></p>
+		<?php endif ?>
+
 		<p class="sdpsemester"><b>Date: </b><?php echo $this->sdp->semester ?> <?php echo $this->sdp->year ?></p>
 		<p class="sdpreqs"><b>Requirements:</b><br /><?php echo $this->sdp->reqs ?></p>
 		<p class="sdpproblem"><b>Problem:</b><br /><?php echo $this->sdp->problem ?></p>
