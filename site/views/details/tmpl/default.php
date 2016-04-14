3e6b877df71a2b8afc848a2db45df8f33edb8bb7
Grant <grantlauren@wireless-10-145-250-102.public.utexas.edu>
2016-04-14 08:54:45 -0500

    changed courl to url in site detail view

--- site/views/details/tmpl/default.php
+++ site/views/details/tmpl/default.php
@@ -4,7 +4,7 @@
 	<h1><?php echo $this->sdp->title ?></h1>
 	<div class="sdpdetails">
 		<p class="sdpstudents"><b>Students:</b><br /><?php echo $this->sdp->students ?></p>
-		<p class="sdpsponsors"><b>Sponsor:</b><br /><a href="<?php echo $row->courl ?>"><?php echo $this->sdp->name ?></a></p>
+		<p class="sdpsponsors"><b>Sponsor:</b><br /><a href="<?php echo $row->url ?>"><?php echo $this->sdp->name ?></a></p>
 		<p class="sdpsemester"><b>Date:</b><br /><?php echo $this->sdp->semester ?> <?php echo $this->sdp->year ?></p>
 		<p class="sdpreqs"><b>Requirements:</b><br /><?php echo $this->sdp->reqs ?></p>
 		<p class="sdpproblem"><b>Problem:</b><br /><?php echo $this->sdp->problem ?></p>
