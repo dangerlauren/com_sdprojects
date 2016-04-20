a05a6ffb63139f1d4b3a5cc24835bb453e2ed7c8
Jason Mayhew <jmayhew@mail.utexas.edu>
2016-04-20 11:44:51 -0500

    Changed var name

--- admin/models/fields/sponsors.php
+++ admin/models/fields/sponsors.php
@@ -20,7 +20,7 @@ class JFormFieldSponsors extends JFormField
   		}
   		
   			
-	 	$dropdown = JHTML::_('select.genericlist', $options, 'company', null, 'value', 'text', $row->id);
+	 	$dropdown = JHTML::_('select.genericlist', $options, 'company', null, 'value', 'text', $options->id);
 		return $dropdown;
 	
 	}
