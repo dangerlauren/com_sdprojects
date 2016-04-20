ca6de8ae53933a482974bdaf280a49aa23028336
Jason Mayhew <jmayhew@mail.utexas.edu>
2016-04-20 11:33:35 -0500

    Changed selected value for further testing

--- admin/models/fields/sponsors.php
+++ admin/models/fields/sponsors.php
@@ -21,7 +21,7 @@ class JFormFieldSponsors extends JFormField
   		
   		$selected = '26';
   			
-	 	$dropdown = JHTML::_('select.genericlist', $options, 'company', null, 'value', 'text', '26');
+	 	$dropdown = JHTML::_('select.genericlist', $options, 'company', null, 'value', 'text', '42');
 		return $dropdown;
 	
 	}
